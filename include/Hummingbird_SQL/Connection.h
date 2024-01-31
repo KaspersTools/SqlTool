d0a1290991d2441f0d4f992d675e5cf2ae586d76
Kasper de Bruin <bruinkasper@gmail.com>
2024-01-31 18:41:10 +0100

    Add comments for docs

--- include/Hummingbird_SQL/Connection.h
+++ include/Hummingbird_SQL/Connection.h
@@ -49,34 +49,137 @@ namespace HummingBird::Sql {
   };
 
   /**
-   *
+    * @brief Connection class for connecting to a MySQL database
+    * @details This class is used to connect to a MySQL database and fetch the schemas, tables, columns and rows.
    */
   class Connection {
 public:
     Connection();
     ~Connection();
 
-    // connection functions
+    /*
+     * @brief Connect to a MySQL database
+     * @details Connect to a MySQL database with the given hostname, username, password, schemaName and port.
+     * @param hostname The hostname of the MySQL database
+     * @param username The username of the MySQL database
+     * @param password The password of the MySQL database
+     * @param schemaName The defaultSchemaName of the MySQL database
+     * @param port The port of the MySQL database
+     */
     void connect(const std::string &hostname, const std::string &username, const std::string &password,
                  const std::string &schemaName, uint16_t port);
+
+    /*
+     * @brief Disconnect from the MySQL database
+     * @details Disconnect from the MySQL database
+     */
     void disconnect();
 
+    /*
+     * @brief Fetch all schemas, tables, columns and rows
+     * @details Fetch all schemas
+     * @param fetchTables Fetch all tables
+     * @param fetchColumnsAndRows Fetch all columns and rows
+     * @return void
+     */
     void fetchSchemas(const bool fetchTables, const bool fetchColumnsAndRows);
 
+    /*
+     * @brief Fetch all tables, columns and rows
+     * @details Fetch all tables
+     * @param schema The schema to fetch the tables from
+     * @param fetchColumnsAndRows Fetch all columns and rows
+     * @return void
+     */
     void fetchTables(SchemaInfo &schema, const bool fetchColumnsAndRows);
+
+    /*
+     * @brief Fetch all tables, columns and rows
+     * @details Fetch all tables
+     * @param schemaName The schemaName to fetch the tables from
+     * @param fetchColumnsAndRows Fetch all columns and rows
+     * @return void
+     */
     void fetchTables(const std::string &schemaName, const bool fetchColumnsAndRows);
 
+    /*
+     * @brief Fetch all columns and rows
+     * @details Fetch all columns and rows
+     * @param schema The schema to fetch the columns and rows from
+     * @param table The table to fetch the columns and rows from
+     * @return void
+     */
     void fetchColumns(SchemaInfo &schema, const std::string &tableName);
+
+    /*
+     * @brief Fetch all columns and rows
+     * @details Fetch all columns and rows
+     * @param schemaName The schemaName to fetch the columns and rows from
+     * @param tableName The tableName to fetch the columns and rows from
+     * @return void
+     */
     void fetchColumns(SchemaInfo &schema, TableInfo &table);
+
+    /*
+     * @brief Fetch all columns and rows
+     * @details Fetch all columns and rows
+     * @param schemaName The schemaName to fetch the columns and rows from
+     * @param tableName The tableName to fetch the columns and rows from
+     * @return void
+     */
     void fetchColumns(const std::string &schemaName, const std::string &tableName);
 
+
+    /*
+     * @brief Fetch all rows
+     * @details Fetch all rows
+     * @param schema The schema to fetch the rows from
+     * @param table The table to fetch the rows from
+     * @return void
+     */
     void fetchRows(SchemaInfo &schema, TableInfo &table);
+
+    /*
+     * @brief Fetch all rows
+     * @details Fetch all rows
+     * @param schemaName The schemaName to fetch the rows from
+     * @param tableName The tableName to fetch the rows from
+     * @return void
+     */
     void fetchRows(SchemaInfo &schema, const std::string &tableName);
+
+    /*
+     * @brief Fetch all rows
+     * @details Fetch all rows
+     * @param schemaName The schemaName to fetch the rows from
+     * @param tableName The tableName to fetch the rows from
+     * @return void
+     */
     void fetchRows(const std::string &schemaNames, const std::string &tableName);
 
+    //getters
 
+    /*
+     * @brief Get an schema by name
+     * @param schemaName The name of the schema
+     * @return SchemaInfo The schema
+     */
     const SchemaInfo &getSchema(const std::string &schemaName);
+
+    /*
+     * @brief Get an table by name
+     * @param schema The schema to get the table from
+     * @param tableName The name of the table
+     * @return TableInfo The table
+     */
     const TableInfo &getTable(SchemaInfo &schema, const std::string &tableName);
+
+    /*
+     * @brief Get an table by name
+     * @param schemaName The schemaName to get the table from
+     * @param tableName The name of the table
+     * @return TableInfo The table
+     */
     const TableInfo &getTable(const std::string &schema, const std::string &tableName);
 
     //getters and setters
@@ -89,7 +192,22 @@ public:
     }
 
 private:
+
+    /*
+     * @brief Get an schema by name
+     * @param schemaName The name of the schema
+     * @return SchemaInfo ptr The schema
+     * @return nullptr if the schema is not found
+     */
     SchemaInfo *getSchemaPtr(const std::string &schemaName);
+
+    /*
+     * @brief Get an table by name
+     * @param schema The schema to get the table from
+     * @param tableName The name of the table
+     * @return TableInfo ptr The table
+     * @return nullptr if the table is not found
+     */
     TableInfo *getTablePtr(SchemaInfo &schema, const std::string &tableName);
 
 private:
